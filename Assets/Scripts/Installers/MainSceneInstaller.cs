6794f5c20b83c62423f070b456d383cb055d18d8
khai1412 <khai14122002123@gmail.com>
2024-06-05 10:32:45 +0700

    update installer

--- Assets/Scripts/Installers/MainSceneInstaller.cs
+++ Assets/Scripts/Installers/MainSceneInstaller.cs
@@ -1,36 +1,47 @@
 ﻿namespace Installers
 {
+    using GameFoundation.Scripts.Utilities.Extension;
+    using Runtime.Elements.Entities.Castles;
+    using Runtime.Managers;
+    using Runtime.Managers.Base;
     using Runtime.StateMachines.GameStateMachine;
+    using Runtime.StateMachines.GameStateMachine.Interfaces;
+    using Runtime.StateMachines.StateMachineBase.Controller;
+    using Runtime.StateMachines.StateMachineBase.Interface;
     using Zenject;
 
     public class MainSceneInstaller : MonoInstaller<MainSceneInstaller>
     {
         public override void InstallBindings()
         {
-            this.BindLocalData();
+            this.BindStateMachine<IGameState,GameStateMachine>();
+            this.BindAllManager();
+            this.BindElement();
             GameStateMachineInstaller.Install(this.Container);
-            //this.Container.Bind<AppService>().FromNewComponentOnNewGameObject().AsSingle().NonLazy();
         }
 
-        private void BindLocalData()
+        private void BindElement()
         {
-            // ReflectionExtension.GetAllDerivedTypes<ILocalDataController>().ForEach(type =>
-            // {
-            //     this.Container.Bind(type).AsCached();
-            // });
-            //
-            // ReflectionExtension.GetAllDerivedTypes<ILocalData>().ForEach(type =>
-            // {
-            //     var cachedData = this.Container.Resolve<HandleLocalDataService>().GetDataFromCached(type);
-            //     if ((cachedData as IDataHaveController)?.ControllerType is { } controllerType)
-            //     {
-            //         this.Container.Bind(type).FromInstance(cachedData).WhenInjectedInto(controllerType);
-            //     }
-            //     else
-            //     {
-            //         this.Container.Bind(type).FromInstance(cachedData).AsCached();
-            //     }
-            // });
+            this.Container.BindFactory<CastleModel, CastlePresenter, CastlePresenter.Factory>().AsCached()
+                .WhenInjectedInto<CastleManager>();
+        }
+
+        private void BindStateMachine<TState, TStateMachine>() where TState : IState where TStateMachine : StateMachine
+        {
+            this.Container.Bind<IState>()
+                .To(convention => convention.AllNonAbstractClasses().DerivingFrom<TState>())
+                .AsSingle()
+                .WhenInjectedInto<TStateMachine>()
+                .NonLazy();
+            this.Container.BindInterfacesAndSelfTo<TStateMachine>().AsSingle().NonLazy();
+        }
+
+        private void BindAllManager()
+        {
+            foreach (var type in ReflectionUtils.GetAllDerivedTypes<IElementManager>())
+            {
+                if(!type.IsAbstract) this.Container.BindInterfacesAndSelfTo(type).AsCached().NonLazy();
+            }
         }
     }
 }
