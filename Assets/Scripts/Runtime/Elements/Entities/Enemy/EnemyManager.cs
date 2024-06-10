a010a30f89fc68ee7aeb985cca016b33b541b823
Nguyen Dinh Quang Anh <anhndqhe161737@fpt.edu.vn>
2024-06-10 13:52:28 +0700

    [WIP] spawn enemy

--- Assets/Scripts/Runtime/Elements/Entities/Enemy/EnemyManager.cs
+++ Assets/Scripts/Runtime/Elements/Entities/Enemy/EnemyManager.cs
@@ -1,22 +1,22 @@
-﻿namespace Runtime.Elements.Entities.Enemy
-{
-    using Runtime.Elements.Base;
-    using Runtime.Managers.Base;
-
-    public class EnemyManager : BaseElementManager<BaseEnemyModel, BaseEnemyPresenter, BaseEnemyView>
-    {
-        public EnemyManager(BaseElementPresenter<BaseEnemyModel, BaseEnemyView, BaseEnemyPresenter>.Factory factory) : base(factory)
-        {
-        }
-        public override void Initialize()        {  }
-        public override void DisposeAllElement()
-        {
-            var cache = this.entities.ToArray();
-            this.entities.Clear();
-            foreach (var entity in cache)
-            {
-                entity.Dispose();
-            }
-        }
-    }
-}
+﻿// namespace Runtime.Elements.Entities.Enemy
+// {
+//     using Runtime.Elements.Base;
+//     using Runtime.Managers.Base;
+//
+//     public class EnemyManager : BaseElementManager<BaseEnemyModel, BaseEnemyPresenter, BaseEnemyView>
+//     {
+//         public EnemyManager(BaseElementPresenter<BaseEnemyModel, BaseEnemyView, BaseEnemyPresenter>.Factory factory) : base(factory)
+//         {
+//         }
+//         public override void Initialize()        {  }
+//         public override void DisposeAllElement()
+//         {
+//             var cache = this.entities.ToArray();
+//             this.entities.Clear();
+//             foreach (var entity in cache)
+//             {
+//                 entity.Dispose();
+//             }
+//         }
+//     }
+// }
