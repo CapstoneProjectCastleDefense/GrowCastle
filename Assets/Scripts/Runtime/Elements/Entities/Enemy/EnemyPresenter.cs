3c1dde34464c066c9a8a073c9226e10ddc8f89f4
Nguyen Dinh Quang Anh <anhndqhe161737@fpt.edu.vn>
2024-06-12 00:57:47 +0700

    fix warning in EnemyPresenter.cs

--- Assets/Scripts/Runtime/Elements/Entities/Enemy/EnemyPresenter.cs
+++ Assets/Scripts/Runtime/Elements/Entities/Enemy/EnemyPresenter.cs
@@ -1,5 +1,6 @@
 ﻿namespace Runtime.Elements.Entities.Enemy
 {
+    using System;
     using System.Linq;
     using Cysharp.Threading.Tasks;
     using GameFoundation.Scripts.Utilities.ObjectPool;
@@ -11,33 +12,38 @@
 
     public class EnemyPresenter : BaseElementPresenter<EnemyModel, EnemyView, EnemyPresenter>, IEnemyPresenter
     {
+        private const string AttackAnimName = "Attack";
+        private const string DeathAnimName  = "Death";
         protected EnemyPresenter(EnemyModel model, ObjectPoolManager objectPoolManager) : base(model, objectPoolManager) { }
+
         public void Attack(ITargetable target)
         {
-            this.View.Animator.SetTrigger("Attack");
+            this.View.Animator.SetTrigger(AttackAnimName);
             target.OnGetHit(this.Model.GetStat<float>(StatEnum.Attack));
         }
+
         public ITargetable FindTarget() { return null; }
+
         public void OnGetHit(float damage)
         {
             var currentHealth = this.Model.GetStat<float>(StatEnum.Health);
             currentHealth -= damage;
             this.Model.SetStat(StatEnum.Health, currentHealth);
         }
-        
 
         public void OnDeath()
         {
-            this.View.Animator.SetTrigger("Death");
-            var clip = this.View.Animator.runtimeAnimatorController.animationClips.First(x => x.name == "Death");
-            UniTask.Delay((int)(clip.length * 1000)).ContinueWith(this.Dispose);
-            this.Dispose();
+            this.View.Animator.SetTrigger(DeathAnimName);
+            var clip = this.View.Animator.runtimeAnimatorController.animationClips.First(x => x.name == DeathAnimName);
+            UniTask.Delay(TimeSpan.FromSeconds(clip.length)).ContinueWith(this.Dispose).Forget();
         }
+
         protected override UniTask<GameObject> CreateView()
         {
             var res = this.ObjectPoolManager.Spawn(this.Model.AddressableName);
             return res;
         }
+
         public override void Dispose() { this.ObjectPoolManager.Recycle(this.View); }
     }
 }
