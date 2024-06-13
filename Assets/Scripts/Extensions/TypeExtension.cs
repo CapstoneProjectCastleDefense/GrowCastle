bd54017954aa4a5f06f5edc17fee55962eb2c98c
TungLV-GOG1071 <goldendarkness1071@gmail.com>
2024-06-13 10:23:31 +0700

    Update Tag, Layers, Physic Matrix, Sorting Layer. Update BaseEnemy.prefab

---
+++ Assets/Scripts/Extensions/TypeExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace Extensions
+{
+    public static class TypeExtension
+    {
+        public static bool IsNullOrEmpty(this string value)
+        {
+            return string.IsNullOrEmpty(value);
+        }
+    }
+}
