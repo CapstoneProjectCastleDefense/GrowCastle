98a08377365c3d9706524ee72132684989c980ad
khai1412 <khai14122002123@gmail.com>
2024-08-10 14:41:29 +0700

    update worm tower

--- Assets/Scripts/Runtime/Elements/EntitySkills/InstantHitSkills/WormAttackSkill.cs
+++ Assets/Scripts/Runtime/Elements/EntitySkills/InstantHitSkills/WormAttackSkill.cs
@@ -6,40 +6,46 @@
     using Models.Blueprints;
     using Models.Tags;
     using Runtime.Elements.Entities.Enemy;
+    using Runtime.Elements.Entities.Projectile;
     using Runtime.Interfaces.Entities;
     using Runtime.Interfaces.Skills;
     using Runtime.Managers;
     using Runtime.StaticValues;
     using UnityEngine;
 
-    public class WormAttackSkill : InstantHitSkill<BasicSkillModel>
+    public class WormAttackSkill : BaseProjectileSkill<BaseProjectileSkillModel>
     {
         private readonly IGameAssets       gameAssets;
         private readonly ObjectPoolManager objectPoolManager;
-        private readonly EffectManager     effectManager;
+        private          GameObject        wormAttack;
 
-        public WormAttackSkill(SkillAttackBlueprint skillAttackBlueprint, IGameAssets gameAssets, ObjectPoolManager objectPoolManager, EffectManager effectManager) : base(skillAttackBlueprint)
+        public WormAttackSkill(ProjectileManager projectileManager, IGameAssets gameAssets, ProjectileBlueprint projectileBlueprint, EffectManager effectManager, ObjectPoolManager objectPoolManager)
+            : base(projectileManager, gameAssets, projectileBlueprint, effectManager)
         {
             this.gameAssets        = gameAssets;
             this.objectPoolManager = objectPoolManager;
-            this.effectManager     = effectManager;
         }
 
         public override string SkillId { get; set; } = EntitySkillName.WormNormalAttack;
 
         protected override void InternalActivate()
         {
-            var wormPrefab = this.gameAssets.LoadAssetAsync<GameObject>(this.VFXName).WaitForCompletion();
-            var wormAttack = this.objectPoolManager.Spawn(wormPrefab, new(3f, -2, 0), Quaternion.identity);
-            wormAttack.GetComponent<ObjectCollideEventHelper>().OnColliderTriggerEnter = this.OnWormCollide;
-            wormAttack.GetComponent<ObjectAutoDestroy>().StartAutoDestroy(3);
+            if (this.wormAttack != null)
+            {
+                this.wormAttack.GetComponent<ObjectAutoDestroy>().StartAutoDestroy(2);
+                return;
+            }
+            var wormPrefab = this.gameAssets.LoadAssetAsync<GameObject>("WormAttack").WaitForCompletion();
+            this.wormAttack = this.objectPoolManager.Spawn(wormPrefab, new(3f, -2, 0), Quaternion.identity);
+            this.wormAttack.GetComponentInChildren<ObjectCollideEventHelper>().OnColliderTriggerEnter = this.OnWormCollide;
+            this.wormAttack.GetComponent<ObjectAutoDestroy>().StartAutoDestroy(2);
         }
 
         private void OnWormCollide(GameObject collideObj, GameObject caster)
         {
             if (collideObj.layer != LayerMask.NameToLayer("Enemy")) return;
             var enemyPresenter = collideObj.GetComponentInParent<EnemyView>().Presenter;
-            this.effectManager.AddEffectToTarget((ITargetable)enemyPresenter, new InstantDamageTag() { Damage = this.Damage });
+            this.effectManager.AddEffectToTarget((ITargetable)enemyPresenter, new InstantDamageTag() { Damage = this.Model.Damage });
             this.effectManager.AddEffectToTarget((ITargetable)enemyPresenter, new SlowTag() { Duration        = 0.5f, Timer = 0 });
         }
     }
