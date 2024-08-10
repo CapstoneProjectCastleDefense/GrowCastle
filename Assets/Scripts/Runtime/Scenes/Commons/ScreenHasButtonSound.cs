16dbd0773a204836b52c325ba9f24a0d2e0a8eba
anhndq_home <quanganh01082002@gmail.com>
2024-08-10 16:25:07 +0700

    add button sound to all screen

---
+++ Assets/Scripts/Runtime/Scenes/Commons/ScreenHasButtonSound.cs
@@ -0,0 +1,20 @@
+﻿namespace Runtime.Scenes.Commons
+{
+    using GameFoundation.Scripts.Utilities;
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    public class ScreenHasButtonSound : MonoBehaviour
+    {
+        private void Awake()
+        {
+            var buttons = this.GetComponentsInChildren<Button>();
+            foreach (var button in buttons)
+            {
+                button.onClick.AddListener(this.PlayButtonClickSound);
+            }
+        }
+
+        private void PlayButtonClickSound() { AudioService.Instance.PlaySound("button_sfx"); }
+    }
+}
