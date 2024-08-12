ae67247f2039dbe103e45a66feae7119062ee049
TungLV <76164212+GOG1071@users.noreply.github.com>
2024-08-12 15:35:18 +0700

    Get Stat From hero and equipment (#96)

--- Assets/Scripts/Runtime/Managers/TowerManager.cs
+++ Assets/Scripts/Runtime/Managers/TowerManager.cs
@@ -14,7 +14,8 @@
         private readonly HeroBlueprint           heroBlueprint;
         private readonly HeroLocalDataController heroLocalDataController;
         private readonly HeroConfigBlueprint     heroConfigBlueprint;
-        public TowerManager(BaseElementPresenter<TowerModel, TowerView, TowerPresenter>.Factory factory,HeroBlueprint heroBlueprint, HeroLocalDataController heroLocalDataController,HeroConfigBlueprint heroConfigBlueprint) : base(factory)
+        public TowerManager(BaseElementPresenter<TowerModel, TowerView, TowerPresenter>.Factory factory, HeroBlueprint heroBlueprint,
+            HeroLocalDataController heroLocalDataController, HeroConfigBlueprint heroConfigBlueprint) : base(factory)
         {
             this.heroBlueprint           = heroBlueprint;
             this.heroLocalDataController = heroLocalDataController;
@@ -26,24 +27,20 @@
             var towerRecord = this.heroConfigBlueprint.GetDataById(id);
             var towerPresenter = this.CreateElement(new()
             {
-                Id = id,
+                Id         = id,
                 ParentView = parent,
                 BaseStats = new()
                 {
-                    { StatEnum.Attack, (typeof(float), towerRecord.BaseAttack) },
-                    { StatEnum.AttackSpeed, (typeof(float), towerRecord.BaseAttackSpeed) },
-                    { StatEnum.AttackPriority,(typeof(AttackPriorityEnum), AttackPriorityEnum.Ground)}
+                    { StatEnum.Attack, (typeof(float), towerRecord.BaseStats[StatEnum.Attack]) },
+                    { StatEnum.AttackSpeed, (typeof(float), towerRecord.BaseStats[StatEnum.AttackSpeed]) },
+                    { StatEnum.AttackPriority, (typeof(AttackPriorityEnum), AttackPriorityEnum.Ground) }
                 },
             }).UpdateView();
             towerPresenter.Forget();
         }
 
-        public void ChangeAttackStatusOfAllTower(bool canAttack) {
-            this.entities.ForEach(e => e.SetAttackStatus(canAttack));
-        }
+        public void ChangeAttackStatusOfAllTower(bool canAttack) { this.entities.ForEach(e => e.SetAttackStatus(canAttack)); }
 
-        public override void Initialize() {
-
-        }
+        public override void Initialize() { }
     }
 }
