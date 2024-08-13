5c5efd9a0c693fdbe485ac9a805aebdf1296ffcd
TungLV <76164212+GOG1071@users.noreply.github.com>
2024-08-14 00:28:57 +0700

    new UI for gameplay, inventory (#102)

--- Assets/Scripts/Runtime/Scenes/Popups/LevelUpPopupView.cs
+++ Assets/Scripts/Runtime/Scenes/Popups/LevelUpPopupView.cs
@@ -35,9 +35,6 @@
         public Button               ConfirmBtn;
         public Button               CancelBtn;
         public ItemInventoryAdapter Adapter;
-        public Transform            startPos;
-        public Transform            endPos;
-        public GameObject           viewField;
     }
 
     [PopupInfo(nameof(LevelUpPopupView), isOverlay: true)]
@@ -74,8 +71,6 @@
         }
         public override async UniTask BindData(LevelUpPopupModel popupModel)
         {
-            this.View.viewField.transform.position = this.View.startPos.position;
-            this.View.viewField.transform.DOMove(this.View.endPos.position, 0.5f).SetEase(Ease.InOutQuint);
             var list       = new List<ItemInventoryItemModel>();
             var localData  = this.inventoryLocalDataController.GetItems(ItemType.InventoryResource);
             var canLevelUp = true;
@@ -97,13 +92,6 @@
 
             this.View.ConfirmBtn.interactable = canLevelUp;
             await this.View.Adapter.InitItemAdapter(list, this.diContainer);
-        }
-        public override void CloseView()
-        {
-            this.View.viewField.transform.DOMove(this.View.startPos.position, 0.5f).SetEase(Ease.InOutQuint).onComplete += () =>
-            {
-                base.CloseView();
-            };
         }
     }
 }
