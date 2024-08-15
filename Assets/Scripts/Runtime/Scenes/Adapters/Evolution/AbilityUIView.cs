351ebb411aa67fc21fe0c0ba98d6b39f87c40a73
anhndq_home <quanganh01082002@gmail.com>
2024-08-15 14:07:57 +0700

    add ability icon

--- Assets/Scripts/Runtime/Scenes/Adapters/Evolution/AbilityUIView.cs
+++ Assets/Scripts/Runtime/Scenes/Adapters/Evolution/AbilityUIView.cs
@@ -8,20 +8,35 @@
 
     public class AbilityUIView : TViewMono
     {
+        [SerializeField] private Image  bgImg,iconImg;
         [SerializeField] private Button selectBtn;
 
+
+        public Image  BgImg     => this.bgImg;
+        public Image  IconImg   => this.iconImg;
         public Button SelectBtn => this.selectBtn;
     }
 
     public class AbilityUIPresenter : BaseUIItemPresenter<AbilityUIView, AbilityUIModel>
     {
         public AbilityUIPresenter(IGameAssets gameAssets) : base(gameAssets) { }
+
+        private AbilityUIModel model;
         public override void BindData(AbilityUIModel param)
         {
+            this.model              = param;
+            this.View.BgImg.enabled = this.model.Id == this.model.SelectedId;
             this.View.SelectBtn.onClick.RemoveAllListeners();
-            this.View.SelectBtn.onClick.AddListener(() => this.OnSelected(param));
+            this.View.SelectBtn.onClick.AddListener(() => this.OnSelected(this.model));
+            this.View.IconImg.sprite = this.GameAssets.LoadAssetAsync<Sprite>(this.model.Icon).WaitForCompletion();
         }
 
+        public void UpdateSelectedId(string selectedId)
+        {
+            this.model.SelectedId   = selectedId;
+            this.View.BgImg.enabled = this.model.Id == this.model.SelectedId;
+        }
+        
         private void OnSelected(AbilityUIModel param)
         {
             param.OnSelected?.Invoke(param.Id);
@@ -31,6 +46,7 @@
     public class AbilityUIModel
     {
         public string         Id         { get; set; }
+        public string         Icon       { get; set; }
         public string         SelectedId { get; set; }
         public Action<string> OnSelected { get; set; }
     }
